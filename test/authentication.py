e74cd3ad02902d2261848b96f60bca52184678b2
micael <duartemicael32@gmail.com>
2021-04-10 16:44:12 -0300

    alpha 20

--- test/authentication.py
+++ test/authentication.py
@@ -8,8 +8,8 @@ class AuthenticationTest(TestCase):
 
     # Return a token object
     def test_should_return_auth_object(self):
-        service = AuthenticationService(client_id='9539f566-38cc-4aca-adbc-51636e08b296',
-                                        client_secret='wj46d9vvwa0rxda44nqdqbe2xrs1beg9nogqxuq3er2z3kvedt3js3x8wez98x8uqr5j1fyz42npjf8mefoetyt4csnze4g173i',
+        service = AuthenticationService(client_id='',
+                                        client_secret='',
                                         grant_type='client_credentials')
         service.execute()
         self.token = service.token
--- test/order_test.py
+++ test/order_test.py
@@ -6,8 +6,8 @@ class OrderTest(TestCase):
 
     def setUp(self) -> None:
         self.service = IfoodService(
-            client_id='9539f566-38cc-4aca-adbc-51636e08b296',
-            client_secret='wj46d9vvwa0rxda44nqdqbe2xrs1beg9nogqxuq3er2z3kvedt3js3x8wez98x8uqr5j1fyz42npjf8mefoetyt4csnze4g173i',
+            client_id='',
+            client_secret='',
             grant_type='client_credentials'
         )
 
